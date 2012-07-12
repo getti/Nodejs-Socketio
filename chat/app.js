c4799385dfe07ea0aab12b20bd0c78fbc716aa3c
getti <tmgetti@gmail.com>
2012-07-12 09:25:43 +0900

    Update master

--- chat/app.js
+++ chat/app.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var app = express.createServer();
-// 3000ポートでプログラムを動作させる
 app.listen(process.env.PORT);
-// ソケットを作る
 var socketIO = require('socket.io');
-// クライアントの接続を待つ(IPアドレスとポート番号を結びつけます)
 var io = socketIO.listen(app);
 
-// クライアントが接続してきたときの処理
 io.sockets.on('connection', function(socket) {
   console.log(socket.id + 'が接続しました。');
   // メッセージを受けたときの処理
   socket.on('message', function(msg) {
     console.log(socket.id + "'がメッセージを送信しました。(" + msg + ")");
-    // つながっているクライアント全員に送信
-    socket.emit('message', msg);
-    socket.broadcast.emit('message', msg);
+
+    socket.emit('message', msg);  // 送信元イベント発火
+    socket.broadcast.emit('message', msg);  // ブロードキャスト
   });
   
-  // クライアントが切断したときの処理
   socket.on('disconnect', function(){
     console.log(socket.id + 'が切断しました。');
   });
